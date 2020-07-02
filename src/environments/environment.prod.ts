d81797fd0d462f4c2a15ec64a60e0d007afccbd5
Roddy Arana Ruiz <roddyaru@gmail.com>
2020-07-02 18:15:28 -0500

    enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  services: {
+    baseUrl: 'https://crud-services.herokuapp.com',
+    person: {
+      persons: '/person',
+      id: '/#id'
+    },
+    log: {
+      logs: '/logs',
+      type: '/list/#type',
+      count: '/count/#type',
+      mont: '/mont',
+      proc: '/proc/#type'
+    }
+  }
 };
