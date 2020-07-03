515e0dfdfa9d43a15349078e381f777f0c1cdc5f
Roddy Arana Ruiz <roddyaru@gmail.com>
2020-07-03 10:55:49 -0500

    cors error

--- src/server.js
+++ src/server.js
@@ -1,17 +1,11 @@
+const cors = require('cors');
 const express = require('express');
 const path = require('path');
 
 const ngApp = express();
 
-//CORS Middleware
-app.use(function (req, res, next) {
-//Enabling CORS
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  next();
-});
+ngApp.use(cors());
+ngApp.options('*', cors());
 
 ngApp.use(express.static('./dist/crud-dashboard'));
 
