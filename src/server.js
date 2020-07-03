fd30eb61a1dcd12bfb8510780d0dcd06d56137f9
Roddy Arana Ruiz <roddyaru@gmail.com>
2020-07-03 11:23:13 -0500

    cors error

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ ngApp.use(cors());
 
 ngApp.options('*', cors());
 
-//CORS Middleware
+//CORS Middleware permissions
 ngApp.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
